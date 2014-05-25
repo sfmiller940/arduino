08710692782a1fa9d9c7227f00f1a4f6b9e9011b
Stephen F. Miller <sfm15@columbia.edu>
2014-05-25 16:13:02 -0700

    Beginning of LED strip stuff.

---
+++ LED_Strip_Fade.c
@@ -0,0 +1,50 @@
+// This code was originally hacked from https://learn.adafruit.com/rgb-led-strips/example-code
+ 
+// Define RGB pins
+#define REDPIN 6
+#define GREENPIN 5
+#define BLUEPIN 3
+ 
+#define FADESPEED 5     // make this higher to slow down
+ 
+void setup() {
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+}
+ 
+ 
+void loop() {
+  int r, g, b;
+ 
+  // fade from blue to violet
+  for (r = 0; r < 256; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from violet to red
+  for (b = 255; b > 0; b--) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from red to yellow
+  for (g = 0; g < 256; g++) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+  // fade from yellow to green
+  for (r = 255; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from green to teal
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from teal to blue
+  for (g = 255; g > 0; g--) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+}
