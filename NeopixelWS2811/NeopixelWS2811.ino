ce23bc6ff4cb1054d44efafd734048528c2f6d91
sfmiller940 <nineforty@gmail.com>
2018-03-25 20:09:37 -0700

    Basic rainbow swirl

--- NeopixelWS2811/NeopixelWS2811.ino
+++ NeopixelWS2811/NeopixelWS2811.ino
@@ -4,9 +4,13 @@ CRGBArray<NUM_LEDS> leds;
 
 void setup() { FastLED.addLeds<NEOPIXEL, 7>(leds, NUM_LEDS); }
 
-void loop() { 
+void loop(){
   static uint8_t hue=0;
-  leds(0,NUM_LEDS/2 - 1).fill_rainbow(hue++);
-  leds(NUM_LEDS/2, NUM_LEDS-1) = leds(NUM_LEDS/2-1,0);
-  FastLED.delay(30);
+  for(int dot = 0; dot < NUM_LEDS; dot++) { 
+    leds[dot] = CHSV( hue + (255 * dot / NUM_LEDS), 255, 255);
+  }
+  FastLED.show();
+  delay(10);
+  hue++;
 }
+
