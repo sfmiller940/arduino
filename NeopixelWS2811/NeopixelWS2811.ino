0936d1b8084d4037b44beeaf9d55a1a04cb7c533
sfmiller940 <nineforty@gmail.com>
2018-03-25 20:27:43 -0700

    Small change

--- NeopixelWS2811/NeopixelWS2811.ino
+++ NeopixelWS2811/NeopixelWS2811.ino
@@ -7,10 +7,10 @@ void setup() { FastLED.addLeds<NEOPIXEL, 7>(leds, NUM_LEDS); }
 void loop(){
   static uint8_t hue=0;
   for(int dot = 0; dot < NUM_LEDS; dot++) { 
-    leds[dot] = CHSV( hue + (255 * dot / NUM_LEDS), 255, 255);
+    leds[dot] = CHSV( (hue * ( 1 - ( 2 * ( dot % 2 ) ) ) ) + (255 * dot / NUM_LEDS), 255, 255);
   }
   FastLED.show();
-  delay(10);
+  delay(20);
   hue++;
 }
 
